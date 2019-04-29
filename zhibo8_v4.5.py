8e799de47ec14984d4a63c3aa524f9d5a218104f
xdream <xdream@gmail.com>
2019-04-29 22:06:26 +0800

    exe tested, but still cant pack to one file.

--- zhibo8_v4.5.py
+++ zhibo8_v4.5.py
@@ -2,7 +2,7 @@ import eel
 import os
 import re
 import requests
-import time
+import datetime
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -32,24 +32,26 @@ def getHtml(url="https://www.zhibo8.cc/"):
 
 def reform(result):  # 整理比赛数据格式
     sort = [result[1], result[0], result[2]]
-    timeLeft = time.mktime(time.strptime(sort[0], '%Y-%m-%d %H:%M')) - time.time()
-    currentDay = time.strftime('%d', time.localtime())
-    listDay = sort[0][8:10]
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    theDayAfterTomorrow = today + datetime.timedelta(days=2)
+    listDay = sort[0][:10]
     listTime = sort[0][-5:]
-    if timeLeft < 82800 and "00:00" <= listTime <= "05:00":  # 判断时间是否需要替换为汉字
-        sort[0] = "今夜 " + listTime
-    elif timeLeft < 82800 and currentDay == listDay:  # 判断时间是否需要替换为汉字
+    night = True if "00:00" <= listTime <= "05:00" else False
+    if listDay == str(today):  # 判断时间是否需要替换为汉字
         sort[0] = "今天 " + listTime
-    elif timeLeft < 144000 and "00:00" <= listTime <= "05:00":
-        sort[0] = "明晚 " + listTime
-    elif timeLeft < 144000:
+    elif listDay == str(tomorrow) and night:  # 判断时间是否需要替换为汉字
+        sort[0] = "今夜 " + listTime
+    elif listDay == str(tomorrow):
         sort[0] = "明天 " + listTime
+    elif listDay == str(theDayAfterTomorrow) and night:
+        sort[0] = "明晚 " + listTime
     return sort
 
 
 def splitTeamInfo(gameInfoList):
-    nonTeam = ['欧联杯', '足球', '篮球', 'NBA', 'CBA', '英超', '西甲', '荷甲', '待定', '中超', '亚冠', '欧冠',
-               '中甲', '足协杯']
+    nonTeam = ['欧联杯', '足球', '篮球', 'NBA', 'CBA', '英超', '西甲', '荷甲', '待定',
+               '中超', '亚冠', '欧冠', '中甲', '足协杯', 'MLB', 'MLB常规赛', '英格兰橄榄球超级联赛']
     giveup = ['篮球', '足球', 'F1', '其他']
     gameInfo = gameInfoList.split(',')
     temp1 = [i for i in gameInfo if i not in nonTeam and i not in giveup]
